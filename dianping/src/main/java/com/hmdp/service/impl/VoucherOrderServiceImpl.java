d585898f2f515e36b6080b00b50c868f666c30dd
Oh...Yeah!!! <614988210@qq.com>
2024-03-19 00:14:40 +0800

    使用乐观锁的CAS法解决优惠券秒杀的超卖问题，同时进行优化，降低了多线程并发产生的数据不一致问题，大大提高了成功率

--- dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
+++ dianping/src/main/java/com/hmdp/service/impl/VoucherOrderServiceImpl.java
@@ -60,7 +60,9 @@ public class VoucherOrderServiceImpl extends ServiceImpl<VoucherOrderMapper, Vou
         //5，扣减库存
         boolean success = seckillVoucherService.update()
                 .setSql("stock= stock -1")
-                .eq("voucher_id", voucherId).update();
+                .eq("voucher_id", voucherId)
+                .gt("stock", 0)     //where id = ? and stock > 0
+                .update();
         //扣减库存
         if (!success) {
             return Result.fail("库存不足！");
